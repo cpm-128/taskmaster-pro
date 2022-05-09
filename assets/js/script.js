7ac406d2461ec3d5e23a1feb1d29aecfa9eaa019
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-09 16:53:30 -0400

    added ability to edit task due date

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,6 +85,58 @@ $(".list-group").on("blur" , "textarea" , function() {
   $(this).replaceWith(taskP);
 });
 
+// change due date was clicked
+$(".list-group").on("click" , "span" , function() {
+
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type" , "text")
+    .addClass("form-control")
+    .val(date);
+
+  //swap out element\
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dataInput.trigger("focus");
+
+});
+
+// value of due date was changed
+$(".list-group").on("blur" , "input[type='text']" , function() {
+  //get current text
+  var date = $(this)
+  .val()
+  .trim();
+
+  // get the paren ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-" , "");
+
+  // get the tasks' position in the list of other li elements
+  var index = $(this)
+    .closest("list-group-item")
+    .index();
+
+  // update task in array and re-save to localStorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap class
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+})
 
 
 
